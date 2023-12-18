543c36ced1d78b8a14b43a30ace3af232851399c
Harsh Patel <harshnpatel9@gmail.com>
2023-12-18 20:29:16 +0530

    fix: build path for windows

--- src/utils/getFiles.ts
+++ src/utils/getFiles.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 
 export async function getFiles(dir: string): Promise<string[]> {
   let files: string[];
-  files = (await glob(`build/${dir}/**/*.js`)).map(file => path.resolve(file));
+  files = (await glob(`./build/${dir}/**/*.js`)).map(file => path.resolve(file));
   console.log(`Found ${files.length} files in ${dir}`);
 
   return files;
