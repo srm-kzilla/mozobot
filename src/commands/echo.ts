a14e0886c1292186dbc3b99da668856a8f491ef6
Vijay M <136885873+vijaybalamahalingam@users.noreply.github.com>
2023-12-22 00:40:29 +0530

    Add `/echo` command (#10)
    * feat: add echo command
    
    * fix: echo cmd
    
    * chore: format file

---
+++ src/commands/echo.ts
@@ -0,0 +1,44 @@
+import {
+  ActionRowBuilder,
+  ChannelType,
+  ModalBuilder,
+  SlashCommandBuilder,
+  SlashCommandChannelOption,
+  TextInputBuilder,
+  TextInputStyle,
+} from 'discord.js';
+import { Command } from '../interface';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Announce a message to a channel')
+    .setDMPermission(false)
+    .addChannelOption((option: SlashCommandChannelOption) => {
+      return option
+        .setName('channel')
+        .setDescription('Channel to announce to')
+        .setRequired(true)
+        .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement);
+    }) as SlashCommandBuilder,
+
+  async execute(interaction) {
+    const channelID = (interaction.options.getChannel('channel')?.id || interaction.channelId) as string;
+    const modal = new ModalBuilder().setCustomId(`echo-${channelID}`).setTitle('Echo Modal');
+
+    const Title = new TextInputBuilder()
+      .setCustomId('Title')
+      .setLabel('Provide us with the Title')
+      .setStyle(TextInputStyle.Short);
+    const Description = new TextInputBuilder()
+      .setCustomId('Description')
+      .setLabel('Provide us with some Description')
+      .setStyle(TextInputStyle.Paragraph);
+
+    const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(Title);
+    const secondActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(Description);
+
+    modal.addComponents(firstActionRow, secondActionRow);
+    await interaction.showModal(modal);
+  },
+} as Command;
