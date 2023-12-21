d399230c5479805b93ba06ac45ed8714683b08a5
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2023-12-21 12:39:11 +0530

    feat: add announce command

---
+++ src/commands/announce.ts
@@ -0,0 +1,41 @@
+import {
+    ActionRowBuilder,
+    ChannelType,
+    ModalBuilder,
+    SlashCommandBuilder,
+    SlashCommandChannelOption,
+    TextInputBuilder,
+    TextInputStyle,
+  } from 'discord.js';
+  import { Command } from '../interface';
+  
+  export default {
+    data: new SlashCommandBuilder()
+      .setName('announce')
+      .setDescription('announcement the world something')
+      .setDMPermission(false)
+      .addChannelOption((option: SlashCommandChannelOption) => {
+        return option
+          .setName('channel')
+          .setDescription('Channel to announce to')
+          .setRequired(true)
+          .addChannelTypes(ChannelType.GuildText , ChannelType.GuildAnnouncement);
+      }) as SlashCommandBuilder,
+  
+    async execute(interaction) {
+      const channelID = (interaction.options.getChannel('channel')?.id || interaction.channelId) as string;
+      const modal = new ModalBuilder().setCustomId(`announce-${channelID}`).setTitle('My Modal');
+      const Title = new TextInputBuilder()
+        .setCustomId('Title')
+        .setLabel('Provide us with the Title')
+        .setStyle(TextInputStyle.Short);
+      const Description = new TextInputBuilder()
+        .setCustomId('Description')
+        .setLabel('Provide us with some Description')
+        .setStyle(TextInputStyle.Paragraph);
+      const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(Title);
+      const secondActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(Description);
+      modal.addComponents(firstActionRow, secondActionRow);
+      await interaction.showModal(modal);
+    },
+  } as Command;
---
+++ src/events/handleModal.ts
@@ -0,0 +1,35 @@
+import { ChannelType, EmbedBuilder, Events, Interaction } from 'discord.js';
+
+export default {
+  name: Events.InteractionCreate,
+  once: false,
+
+  async execute(interaction: Interaction) {
+    if (!interaction.isModalSubmit()) return;
+    if (!interaction.guild) return;
+    const [cmd, channelID ]= interaction.customId.split('-')
+    if (!cmd || !channelID) return;
+
+    const channel = interaction.guild.channels.cache.get(channelID);
+
+    if (!channel) {
+      return await interaction.reply({ content: 'Target Channel Not Found', ephemeral: true });
+    }
+
+    if (channel.type !== ChannelType.GuildText && channel.type !== ChannelType.GuildAnnouncement) {
+      return await interaction.reply({ content: 'Inavlid Channel Provided. Please Provide a text channel' });
+    }
+
+    const Title = interaction.fields.getTextInputValue('Title');
+    const Description = interaction.fields.getTextInputValue('Description');
+
+    if (cmd === 'announce') {
+        const embed = new EmbedBuilder().setTitle(Title).setDescription(Description).setColor('#FFFFFF');
+
+        await channel.send({ embeds: [embed] });
+    } else if (cmd === 'echo') {
+        await channel.send({content: `# ${Title}\n${Description}`})
+    }
+    await interaction.reply({ content: `Announcement sent to <#${channel.id}>`});
+  },
+};
