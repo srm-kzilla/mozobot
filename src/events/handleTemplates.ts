ecf3053e7a3d60cbb95d81a9eef334f4219f5231
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2023-12-29 21:30:08 +0530

    chore: rename files

---
+++ src/events/handleTemplates.ts
@@ -0,0 +1,28 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Collection, Events, Interaction } from 'discord.js';
+import { Command } from '../interface';
+import db from '../utils/database';
+import { ObjectId } from 'mongodb';
+
+export default {
+  name: Events.InteractionCreate,
+  once: false,
+
+  async execute(interaction: Interaction, commands: Collection<string, Command>) {
+    if (interaction.isStringSelectMenu()) {
+      if (interaction.customId === 'deleteTemplate') {
+        const templateID = interaction.values[0];
+
+        const collection = (await db()).collection('templates');
+        await collection.deleteOne({  _id: new ObjectId(templateID)});
+        await interaction.reply('Successfully Deleted');
+
+      } else if (interaction.customId === 'chooseTemplate') {
+
+        const templateID = interaction.values[0];
+        await interaction.reply({
+          content: `Use \`/announce\` or \`/echo\` command and provide with this \`${templateID}\` to use it.`,
+        });
+      }
+    }
+  },
+};
