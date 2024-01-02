23a9db45da4fd10c0f57d5f48507239b43b3460a
vijaybalamahalingam <vijaybala.mahalingam@gmail.com>
2024-01-02 17:19:26 +0530

    feat: implement template schema Co-authored-by: Sachin Prasanth <SachinPrasanth777@users.noreply.github.com>

--- src/events/handleModal.ts
+++ src/events/handleModal.ts
@@ -1,22 +1,41 @@
 import { ChannelType, ColorResolvable, EmbedBuilder, Events, Interaction } from 'discord.js';
 import { COLOR } from '../config/constant';
 import db from '../utils/database';
+import { templateSchemaType } from '../types';
 
 export default {
   name: Events.InteractionCreate,
   once: false,
 
   async execute(interaction: Interaction) {
-    if (!interaction.isModalSubmit()) return;
     if (!interaction.guild) return;
-    const Title = interaction.fields.getTextInputValue('Title');
-    const Description = interaction.fields.getTextInputValue('Description');
+    if (!interaction.isModalSubmit()) return;
+    const title = interaction.fields.getTextInputValue('Title');
+    const description = interaction.fields.getTextInputValue('Description');
 
     const [cmd, channelID] = interaction.customId.split('-');
 
+
+
     if (cmd === 'template') {
-      const collection = (await db()).collection('templates');
-      await collection.insertOne({ Title: Title, Description: Description });
+
+      const data = await (await db()).collection('templates').find({guildId: interaction.guildId, isDeleted: false}).toArray();
+      if (data.length >= 25) {
+        return interaction.reply({content: 'You can only have max 25 templates', ephemeral: true})
+      }
+
+      const templateExists = data.some((template) => template.title === title || template.description === description);
+
+      if (templateExists) {
+        return interaction.reply({ content: 'Template with the same title and description already exists', ephemeral: true });
+      }
+      
+      await (await db()).collection<templateSchemaType>('templates').insertOne({
+        title,
+        description,
+        guildId: interaction.guild.id,
+        isDeleted: false
+      });
       await interaction.reply({ content: 'Template added to database!' });
     } else {
       if (!cmd || !channelID) return;
@@ -35,14 +54,14 @@ export default {
 
       if (cmd === 'announce') {
         const embed = new EmbedBuilder()
-          .setTitle(Title)
-          .setDescription(Description)
+          .setTitle(title)
+          .setDescription(description)
           .setColor(COLOR.WHITE as ColorResolvable);
 
         await channel.send({ embeds: [embed] });
         await interaction.reply({ content: `Announcement sent to <#${channel.id}>` });
       } else if (cmd === 'echo') {
-        await channel.send({ content: `# ${Title}\n${Description}` });
+        await channel.send({ content: `# ${title}\n${description}` });
         await interaction.reply({ content: `Announcement sent to <#${channel.id}>` });
       }
     }
