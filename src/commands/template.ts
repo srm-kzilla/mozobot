9369394d40290216e84340be8fa3ac7e0e10dc57
vijaybalamahalingam <vijaybala.mahalingam@gmail.com>
2024-01-02 17:48:14 +0530

    feat: handle empty templates

--- src/commands/template.ts
+++ src/commands/template.ts
@@ -9,6 +9,8 @@ import {
 } from 'discord.js';
 import { Command } from '../interface';
 import db from '../utils/database';
+import { templateSchemaType } from '../types';
+import { ObjectId } from 'mongodb';
 export default {
   data: new SlashCommandBuilder()
     .setName('template')
@@ -25,6 +27,7 @@ export default {
     ) as SlashCommandBuilder,
 
   async execute(interaction) {
+    if (!interaction.guild) return;
     const subcommand = interaction.options.getSubcommand();
 
     if (subcommand === 'create') {
@@ -45,14 +48,25 @@ export default {
       modal.addComponents(firstActionRow, secondActionRow);
       await interaction.showModal(modal);
     } else if (subcommand === 'delete') {
-      const data = await (await db()).collection('templates').find().toArray();
+      const data = await (await db())
+        .collection('templates')
+        .find({ guildId: interaction.guildId, isDeleted: false })
+        .toArray();
+
+      // !data
+      if (data.length === 0) {
+        await interaction.reply({
+          content: 'No Templates found! use `/template create` to create one!',
+          ephemeral: true,
+        });
+      }
 
       const templatesData = data.map(data => ({
-        label: data.Title.slice(0, 50),
-        description: data.Description.slice(0, 50),
+        label: data.title.slice(0, 50),
+        description: data.description.slice(0, 50),
         value: data._id.toString(),
       }));
-      console.log(templatesData);
+
       const selectMenu = new StringSelectMenuBuilder()
         .setCustomId(`deleteTemplate`)
         .setPlaceholder('Select a template to delete')
@@ -61,10 +75,26 @@ export default {
       const actionRow = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(selectMenu);
       await interaction.reply({ content: 'Select a template to delete:', components: [actionRow], ephemeral: true });
     } else if (subcommand === 'list') {
-      const data = await (await db()).collection('templates').find().toArray();
+      let data = await (
+        await db()
+      )
+        .collection('templates')
+        .find({
+          guildId: interaction.guildId,
+          isDeleted: false,
+        })
+        .toArray();
+
+      if (data.length === 0) {
+        await interaction.reply({
+          content: 'No Templates found! use `/template create` to create one!',
+          ephemeral: true,
+        });
+      }
+
       const templatesData = data.map(data => ({
-        label: data.Title.slice(0, 50),
-        description: data.Description.slice(0, 50),
+        label: data.title.slice(0, 50),
+        description: data.description.slice(0, 50),
         value: data._id.toString(),
       }));
 
