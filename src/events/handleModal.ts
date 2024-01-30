0c09affa1f835d61d98a99780d1e6d6f734aca19
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-01-30 21:32:30 +0530

    fix: lint

--- src/events/handleModal.ts
+++ src/events/handleModal.ts
@@ -175,7 +175,7 @@ export default {
         }
         const channel = await interaction.client.channels.fetch(channelId);
         if (!channel) return;
-        // @ts-ignore
+        // @ts-expect-error
         message = await channel.messages.fetch(messageId);
         if (type === "announce") {
           const images = (interaction.fields.getTextInputValue("image") || "")
