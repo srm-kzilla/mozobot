91902ef0a480590be790a2eb990279c16c3c5241
alvinbengeorge <alvinallen333@gmail.com>
2024-03-01 19:45:34 +0530

    format: use prettier to format code

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,17 +4,14 @@ import { getCommandData } from "../utils";
 export default {
   data: new SlashCommandBuilder().setName("help").setDescription("Get help with using the bot"),
   async execute(interaction: ChatInputCommandInteraction<CacheType>) {
-    const commands = getCommandData().map(command => Object(command));    
+    const commands = getCommandData().map(command => Object(command));
     const embed = new EmbedBuilder().setTitle("Help");
     commands.forEach(command => {
-      console.log(command);
-      embed.addFields(
-        {
-          name: `/${command.name}`,
-          value: command.description,
-          inline: true,
-        },
-      );
+      embed.addFields({
+        name: `/${command.name}`,
+        value: command.description,
+        inline: true,
+      });
     });
 
     await interaction.reply({ embeds: [embed] });
