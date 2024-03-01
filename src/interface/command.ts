7ce92a89fc1dd3f60ee625ecb58369d934ccf193
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-03-01 19:52:23 +0530

    fix: fix ci

--- src/interface/command.ts
+++ src/interface/command.ts
@@ -2,7 +2,7 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export interface Command {
   data: SlashCommandBuilder;
-  isMod: Boolean;
+  isMod: boolean;
 
   execute(interaction: ChatInputCommandInteraction): void;
 }
